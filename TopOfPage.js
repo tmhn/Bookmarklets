5d3e1aabe735a8f58fb3e0a360ac35065ac169b0
Tom Hanson <iTomHanson@gmail.com>
2015-03-17 09:19:16 +0000

    Create TopOfPage.js
    Initial commit

---
+++ TopOfPage.js
@@ -0,0 +1,7 @@
+//Block
+javascript:(function(){
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+})();
+
+//Inline
+javascript: void(function(){$("html, body").animate({ scrollTop: 0 }, "slow");})();
