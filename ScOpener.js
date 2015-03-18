45dcefc09624c8844c11b13a1b7e116594d6c14b
Tom Hanson <iTomHanson@gmail.com>
2015-03-18 11:50:05 +0000

    Create ScOpener.js

---
+++ ScOpener.js
@@ -0,0 +1,8 @@
+//---Initial --- Requires you to be on the page of the selected Site Confidence test e.g. "Test > Speed Analysis > Weekly Average > 24 Weeks".
+//Initial block
+javascript:(function(){
+  window.open("https://portal.siteconfidence.co.uk"+$("img[usemap='#map1']").attr("src"));
+})();
+
+//Initial inline
+javascript:(function(){window.open("https://portal.siteconfidence.co.uk"+$("img[usemap='#map1']").attr("src"));})();
