c3a29826248387767d1188a5426071ee51e9916d
Tom Hanson <iTomHanson@gmail.com>
2015-03-18 08:12:54 +0000

    Create InstaOpener.js

---
+++ InstaOpener.js
@@ -0,0 +1,8 @@
+//Block
+javascript:(function(){
+    window.open(instaImageUrl=$("div[class='lfFrame Frame Image']").attr("src"));
+  })
+();
+
+//Inline
+javascript:(function(){window.open(instaImageUrl=$("div[class='lfFrame Frame Image']").attr("src"));})();
